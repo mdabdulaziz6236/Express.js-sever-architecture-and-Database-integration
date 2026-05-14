8c6883dbe070b80870fa72c2bc40b04a90c0f804
MD. ABDUL AZIZ <md.abdulaziz6236@gmail.com>
2026-05-14 09:51:27 +0600

    Updated code if user want to update single one

--- src/server.ts
+++ src/server.ts
@@ -118,7 +118,11 @@ app.put('/api/users/:id', async (req: Request, res: Response) => {
   const id = req.params.id;
   const { name, password, age, is_active, } = req.body;
   const result = await pool.query(`
-    UPDATE users SET name=$1,password=$2,age=$3,is_active=$4
+    UPDATE users SET
+     name=COALESCE($1,name),
+     password=COALESCE($2,password),
+     age=COALESCE($3,age),
+     is_active=COALESCE($4,is_active)
     WHERE id=$5 RETURNING *
     `, [name, password, age, is_active, id])
   if (result.rows.length === 0) {
