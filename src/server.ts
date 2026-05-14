09089163e2e153d0999095784f073cb9b92e4aed
MD. ABDUL AZIZ <md.abdulaziz6236@gmail.com>
2026-05-14 06:25:58 +0600

    POST Method

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { type Application, type Request, type Response } from 'express' 
 const app :Application = express()
 const port = 5000
+app.use(express.json())
 
 app.get('/', (req:Request, res:Response) => {
 //   res.send('Express Server is running!')
@@ -11,6 +12,9 @@ app.get('/', (req:Request, res:Response) => {
   })
 })
 
+app.post("/", async(req:Request, res:Response)=>{
+  console.log(req.body)
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
