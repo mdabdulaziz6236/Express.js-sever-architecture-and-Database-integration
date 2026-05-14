1dd141798a368fba8a2c97e759e7220f6ae63570
MD. ABDUL AZIZ <md.abdulaziz6236@gmail.com>
2026-05-14 06:35:53 +0600

    POST Method req.body as res

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { type Application, type Request, type Response } from 'express'
 const app :Application = express()
 const port = 5000
 app.use(express.json())
+app.use(express.text())
+app.use(express.urlencoded({extended:true})) // usaslly nested data ney na tai {extended: true}
 
 app.get('/', (req:Request, res:Response) => {
 //   res.send('Express Server is running!')
@@ -13,7 +15,12 @@ app.get('/', (req:Request, res:Response) => {
 })
 
 app.post("/", async(req:Request, res:Response)=>{
-  console.log(req.body)
+  // console.log(req.body)
+  const {name,email,password} = req.body;
+  res.status(201).json({
+    message:"Created",
+    data:{name,email}
+  })
 })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
