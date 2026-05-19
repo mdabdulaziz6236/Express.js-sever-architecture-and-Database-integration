793a6b7d79bf681fd9b8517ff27a3befbbff0001
MD. ABDUL AZIZ <md.abdulaziz6236@gmail.com>
2026-05-19 12:53:21 +0600

    Added Simple middleware

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,26 @@ import { pool } from './db'
 import { userRoute } from './modules/user/user.route'
 import { profileRoute } from './modules/profile/profile.route'
 import { authRoute } from './modules/auth/auth.route'
+import fs from 'fs'
 const app: Application = express()
 app.use(express.json())
 app.use(express.text())
 app.use(express.urlencoded({ extended: true })) // usaslly nested data ney na tai {extended: true}
+app.use((req, res, next) => {
+    const currentTime = new Date().toLocaleString();
+    const log = `\n========================================
+Method : ${req.method}
+URL    : ${req.url}
+Time   : ${currentTime}
+========================================\n`;
+    fs.appendFile('logger.txt', log, (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+
+    next();
+});
 
 
 
