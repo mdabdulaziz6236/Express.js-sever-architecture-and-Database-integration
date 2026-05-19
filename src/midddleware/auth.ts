e985e994adfe3050c4697221094519248c8145af
MD. ABDUL AZIZ <md.abdulaziz6236@gmail.com>
2026-05-19 16:45:25 +0600

    Auth middleware validation

--- src/midddleware/auth.ts
+++ src/midddleware/auth.ts
@@ -1,14 +1,35 @@
 import type { NextFunction, Request, Response } from "express"
-
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import { pool } from "../db";
 const auth = () => {
     return async (req: Request, res: Response, next: NextFunction) => {
-        // console.log(req.headers.authorization)
         const token = req.headers.authorization;
-        if(!token){
+
+        console.log(token)
+        if (!token) {
             res.status(401).json({
-            success: false,
-            message: "Unauthorized access."
-        })
+                success: false,
+                message: "Unauthorized access."
+            })
+        }
+
+        const decoded = jwt.verify(token as string, config.secret as string) as JwtPayload
+        const userData = await pool.query(` 
+            SELECT * FROM users WHERE email=$1
+            `, [decoded.email])
+        const user = userData.rows[0]
+        if (userData.rowCount === 0) {
+            res.status(401).json({
+                success: false,
+                message: "User not found!"
+            })
+        }
+        if (!user.is_active) {
+            res.status(403).json({
+                success: false,
+                message: "Forbidden!"
+            })
         }
         next()
     }
